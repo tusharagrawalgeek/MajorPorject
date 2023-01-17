a008e75de1542a7670466d19113ba40f596d3636
tusharagrwall <tusharagrawalgeek@gmail.com>
2023-01-18 00:03:52 +0530

    final

--- src/App.js
+++ src/App.js
@@ -4,15 +4,11 @@ import Starter from './Starter.jsx';
 import React from 'react';
 import SideBar  from './SideBar';
 import Content from './Content';
-import Login from './Login.jsx';
 const click=false;
 function App(){
   return (
     <>
     <div>
-      <Login/>
-    </div>
-    {/* <div>
       <table style={{borderSpacing:0, borderPadding:0}}>
         <tr>
           <td style={{padding:"0", verticalAlign:"top"}}>
@@ -27,7 +23,15 @@ function App(){
           </td>
         </tr>
       </table>
+    </div>
+    {/* <div style={{
+      background:" yellow",
+      fontFamily:"Source Sans Pro"
+      }}>
+      
+
     </div> */}
+    
     </>
   );
 }
